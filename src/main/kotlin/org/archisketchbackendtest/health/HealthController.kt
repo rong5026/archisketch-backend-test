38f838c705a00c9bedfb85c159685d58324d7f6d
rong5026 <rong5026@naver.com>
2025-04-03 18:40:12 +0900

    feat: health check 구현

---
+++ src/main/kotlin/org/archisketchbackendtest/health/HealthController.kt
@@ -0,0 +1,13 @@
+package org.archisketchbackendtest.health
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthController {
+
+    @GetMapping("/health")
+    fun healthCheck(): String {
+        return "1"
+    }
+}
