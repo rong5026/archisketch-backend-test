bb56455912545f8d04b5950a2c8145ea2e8d80b9
rong5026 <rong5026@naver.com>
2025-04-03 17:52:21 +0900

    chore: init project

---
+++ src/main/kotlin/org/archisketchbackendtest/ArchisketchBackendTestApplication.kt
@@ -0,0 +1,11 @@
+package org.archisketchbackendtest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArchisketchBackendTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<ArchisketchBackendTestApplication>(*args)
+}
---
+++ src/test/kotlin/org/archisketchbackendtest/ArchisketchBackendTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.archisketchbackendtest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArchisketchBackendTestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
