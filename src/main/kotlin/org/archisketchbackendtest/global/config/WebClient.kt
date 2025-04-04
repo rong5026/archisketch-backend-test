2fb41333a61bb3e6a5545668c4f61b8a7b06a537
rong5026 <rong5026@naver.com>
2025-04-04 15:10:40 +0900

    style: global 패키지 생성 후 업데이트

--- src/main/kotlin/org/archisketchbackendtest/global/config/WebClient.kt
+++ src/main/kotlin/org/archisketchbackendtest/global/config/WebClient.kt
@@ -1,4 +1,4 @@
-package org.archisketchbackendtest.config
+package org.archisketchbackendtest.global.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
